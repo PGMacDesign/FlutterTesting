fadc8235b03f0337606325fc0a66a34fcdbc737d
PGMacDesign <PGMacDesign@gmail.com>
2019-05-29 10:10:25 -0700

    Moving to new data entirely

---
+++ android/app/src/main/kotlin/pgmacdesign/flutter_testing_v2/MainActivity.kt
@@ -0,0 +1,13 @@
+package pgmacdesign.flutter_testing_v2
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
